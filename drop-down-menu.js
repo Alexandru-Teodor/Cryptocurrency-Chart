54d5603a710486b7c088db5f8e78d24b1e2eed76
Alexandru Andries <alexandru.teodor.andries@gmail.com>
2022-06-06 17:08:52 +0300

    deleted unnecessary file after code update

--- drop-down-menu.js
+++
@@ -1,8 +0,0 @@
-// function show(option) {
-//     document.querySelector('.textBox').value = option;
-// }
-
-// let dropdown = document.querySelector('.dropdown');
-// dropdown.addEventListener('click', () => {
-//     dropdown.classList.toggle('active');
-// })
